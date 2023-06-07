d515a522538619dc70b16abeaa4b83590f393706
israelrodrigues01 <ciceroisral428@gmail.com>
2023-06-07 10:55:00 -0300

    crio função dos ramos do tipo alfa

--- tabluex.py
+++ tabluex.py
@@ -1,3 +1,5 @@
+import os
+
 tamatual = 2
 
 ramo = []
@@ -10,11 +12,41 @@ def beta(formula):
 
 
 # Função para criar ramos alfas
-def alfa(formula):
-  print('implica - falso')
-  print('e - verdadeiro')
-  print('ou - falso')
-  print('negação - verdadeiro/falso')
+def alfa(formula, conectivo):
+  primeiro, segundo = formula.split(conectivo, 1)
+  # Implicação - falso
+  if conectivo == '->':
+    primeiro = 'T' + retirarEspaco(primeiro)
+    segundo = 'F' + retirarEspaco(segundo)
+    ramo.append(primeiro)
+    ramo.append(segundo)
+
+  # e - verdadeiro
+  elif conectivo == '^':
+    primeiro = 'T' + retirarEspaco(primeiro)
+    segundo = 'T' + retirarEspaco(segundo)
+    ramo.append(primeiro)
+    ramo.append(segundo)
+
+  # ou - falso
+  elif conectivo == 'v':
+    primeiro = 'F' + retirarEspaco(primeiro)
+    segundo = 'F' + retirarEspaco(segundo)
+    ramo.append(primeiro)
+    ramo.append(segundo)
+
+  # negação - verdadeiro/falso
+  elif conectivo == '-':
+    if formula[0] == 'F':
+      segundo = 'T' + retirarEspaco(segundo)
+    
+    else: 
+      segundo = 'F' + retirarEspaco(segundo)
+
+    ramo.append(segundo)
+  
+  criarFomula(primeiro)
+  criarFomula(segundo)
 
 def retirarEspaco(string):
   return string.strip()
@@ -22,32 +54,42 @@ def retirarEspaco(string):
 def criarFomula(formula):
   if '|-' in formula:  
     primeiro, segundo = formula.split('|-') # todos os primeiros devem ser verdadeiror, e o segundo devem ser falsos;
-    
     segundo = 'F'+ retirarEspaco(segundo)
     ramo.append(segundo)
     criarFomula(segundo)
     
-
   elif '->' in formula:  
-    primeiro,segundo = formula.split('->')
-      
-    ramo.append(segundo)
+    if formula[0] == 'F':
+      alfa(formula[1:], '->')
+    else:
+      beta(formula[1:],'->')
+  
+    
 
   elif '^' in formula:  
-    primeiro,segundo = formula.split('^')
-    ramo.append(segundo)
+    if formula[0] == 'F':
+      beta(formula[1:],'^')
+    else:
+      alfa(formula[1:], '^')
+
+    
 
   elif 'v' in formula:  
-    primeiro,segundo = formula.split('v')
-    ramo.append(segundo)
+    if formula[0] == 'F':
+      alfa(formula[1:], 'v')
+    else:
+      beta(formula[1:],'v')
 
-  elif '-' in formula:  
-    primeiro, segundo = formula.split('-')
-    ramo.append(segundo)
+    
+
+  elif '-' in formula: 
+    alfa(formula, '-')
+    
 
 
-criarFomula('|- r -> c')
+criarFomula('|- -r v -c v c')
 
+os.system('cls')
 print(ramo)
 
 
